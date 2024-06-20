f1c8fdac95933cddd289818fd9439e78a317e49b
Tochka <tochka114@gmail.com>
2024-06-20 19:42:54 +0300

    update-pixabay

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,28 @@
-export function getImg(query) {
+import axios from 'axios';
+import iziToast from 'izitoast';
+
+export async function getImg(query) {
   const BASE_URL = 'https://pixabay.com';
   const END_POINT = '/api/';
-  const params = new URLSearchParams({
-    key: '44403468-b3192a3f07a9f4ae6c998e95a',
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-  const url = `${BASE_URL}${END_POINT}?${params}`;
-
-  return fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      if (!data.hits.length) {
-        throw new Error('No images found');
-      }
-      return data;
+  const url = `${BASE_URL}${END_POINT}`;
+  const options = {
+    params: new URLSearchParams({
+      key: '44403468-b3192a3f07a9f4ae6c998e95a',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    }),
+  };
+  try {
+    const res = await axios.get(url, options);
+    return res.data;
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a search term',
+      position: 'topRight',
     });
-}
+    throw error; 
+  }
+};
